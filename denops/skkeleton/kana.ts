f98a230fa95fdaef1d2fc73df8d701a0bc5316e2
kei-s16 <kei1216s@nifty.com>
2022-12-03 01:13:04 +0900

    add azik keymap

--- denops/skkeleton/kana.ts
+++ denops/skkeleton/kana.ts
@@ -3,12 +3,14 @@ import { distinctBy } from "./deps/std/collections.ts";
 import { assertObject, isString } from "./deps/unknownutil.ts";
 import { functions } from "./function.ts";
 import { romToHira } from "./kana/rom_hira.ts";
+import { azikToHira } from "./kana/azik_hira.ts";
 import { romToZen } from "./kana/rom_zen.ts";
 import type { KanaResult, KanaTable } from "./kana/type.ts";
 import { Cell } from "./util.ts";
 
 const tables: Cell<Record<string, KanaTable>> = new Cell(() => ({
   "rom": romToHira,
+  "azik": azikToHira,
   "zen": romToZen,
 }));
 
